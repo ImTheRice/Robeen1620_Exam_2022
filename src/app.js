cf198b503fd285c192b7648204290eece997d223
Robeen M <maharjanrobeen@gmail.com>
2022-04-21 12:13:50 -0700

    ability to open basic card is in app.js and some style in CSS

--- src/app.js
+++ src/app.js
@@ -1,54 +1,75 @@
 // contact list, don't change this.
-const contactsList = [
-  {
-    name: "Togusa",
-    phone: "555 555-5555",
-    image: "togusa.png",
-    email: "togusa@secnine.com",
-    ID: "0"
-  },
-  {
-    name: "Tachikoma",
-    phone: "555 555-5555",
-    image: "tachikoma.png",
-    email: "tachikoma@secnine.com",
-    ID: "1"
-  },
-  {
-    name: "Saito",
-    phone: "555 555-5555",
-    image: "saito.png",
-    email: "saito@secnine.com",
-    ID: "2"
-  },
-  {
-    name: '"major" Motoko',
-    phone: "555 555-5555",
-    image: "motoko.png",
-    email: "motoko@secnine.com",
-    ID: "3"
-  },
-  {
-    name: "Ishikawa",
-    phone: "555 555-5555",
-    image: "ishikawa.png",
-    email: "ishikawa@secnine.com",
-    ID: "4"
-  },
-  {
-    name: "Batou",
-    phone: "555 555-5555",
-    image: "batou.png",
-    email: "batou@secnine.com",
-    ID: "5"
-  },
-  {
-    name: "Aramaki",
-    phone: "555 555-5555",
-    image: "aramaki.png",
-    email: "aramaki@secnine.com",
-    ID: "6"
-  },
+const contactsList = [{
+        name: "Togusa",
+        phone: "555 555-5555",
+        image: "togusa.png",
+        email: "togusa@secnine.com",
+        ID: "0"
+    },
+    {
+        name: "Tachikoma",
+        phone: "555 555-5555",
+        image: "tachikoma.png",
+        email: "tachikoma@secnine.com",
+        ID: "1"
+    },
+    {
+        name: "Saito",
+        phone: "555 555-5555",
+        image: "saito.png",
+        email: "saito@secnine.com",
+        ID: "2"
+    },
+    {
+        name: '"major" Motoko',
+        phone: "555 555-5555",
+        image: "motoko.png",
+        email: "motoko@secnine.com",
+        ID: "3"
+    },
+    {
+        name: "Ishikawa",
+        phone: "555 555-5555",
+        image: "ishikawa.png",
+        email: "ishikawa@secnine.com",
+        ID: "4"
+    },
+    {
+        name: "Batou",
+        phone: "555 555-5555",
+        image: "batou.png",
+        email: "batou@secnine.com",
+        ID: "5"
+    },
+    {
+        name: "Aramaki",
+        phone: "555 555-5555",
+        image: "aramaki.png",
+        email: "aramaki@secnine.com",
+        ID: "6"
+    },
 ]
 
-// Your code goes here
+const allContact = document.querySelector("#display_all_contacts")
+allContact.addEventListener('click', test)
+
+function test() {
+    console.log('hello')
+}
+
+
+function loadContacts() {
+
+    const lengthNum = Object.keys(contactsList).length
+    let i = 0
+
+    while (i < lengthNum + 1) {
+        console.log(contactsList[i])
+        allContact.insertAdjacentHTML("beforeend",
+            `<div id=individual_contact>${contactsList[i].name}</div>`)
+        i += 1
+
+    }
+}
+
+loadContacts()
